29e1c34641bf30cc85592f20bea767a13b20fdb7
Andy Gelme <andyg@geekscape.org>
2018-05-09 14:39:14 +1000

    Improve joystick controller to include "acceleration".

--- applications/joysticks.py
+++ applications/joysticks.py
@@ -11,10 +11,10 @@ import json
 import aiko.event as event
 
 pins = []
-pin_states = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+pins_active = []
 
-# Pin numbers 32, 33, 34, 35 used for buttons
-# Pin number 13 used for Wi-Fi LED !
+# Pin number 13 used for Wi-Fi LED and one of the joysticks !
+# Pin numbers 32, 33, 34, 35 will be used for buttons
 
 pin_numbers = [
   14, 25, 26, 27,  # controller 0, left  joystick: left, right, down, up
@@ -33,46 +33,47 @@ pin_delta_map = [
   -0.1, 0.1, -0.1, 0.1, -0.1, 0.1, -0.1, 0.1
 ]
 
-axis_value = [ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ]
-
 axis_controller_map = [ 0, 0, 0, 0, 1, 1, 1, 1 ]
 
 axis_name = [ "LH", "LV", "RH", "RV", "LH", "LV", "RH", "RV" ]
 
-def handle_pin(pin):
+axis_value = [ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ]
+
+def handle_pin_change(pin):
   try:
+    pin_index = pins.index(pin)          # Will raise ValueError on unknown pin
+
+    if not pin.value():
+      if not pin in pins_active: pins_active.append(pin)
+    else:
+      if pin in pins_active:
+        pins_active.remove(pin)
+        axis_value[pin_axis_map[pin_index]] = 0.0
+
+  except ValueError:
+    print("ERROR: handle_pin_change(): Unknown Pin argument received")
+
+def handle_pins_active():
+  for pin in pins_active:
     pin_index = pins.index(pin)
-    pin_value = pin.value()
-
-    if pin_value:
-      pin_states[pin_index] = 1
-      return  # Ignore when pin is high
-
-    if pin_states[pin_index] == 0: return  # Ignore if pin was already low
-    pin_states[pin_index] = 0
-
     axis_index = pin_axis_map[pin_index]
     axis_value[axis_index] += pin_delta_map[pin_index]
     if axis_value[axis_index] >  1.0: axis_value[axis_index] =  1.0
     if axis_value[axis_index] < -1.0: axis_value[axis_index] = -1.0
 
-    output_name = axis_name[axis_index]
     output_value = round(axis_value[axis_index], 3)
 
     payload = {}
     payload["controller"] = axis_controller_map[axis_index]
     payload["action"] = "move"
-    payload["axes"] = [(output_name, output_value)]
+    payload["axes"] = [(axis_name[axis_index], output_value)]
     payload_out = json.dumps(payload)
-
     print(payload_out)
-  except ValueError:
-    print("ERROR: handle_pin(): Unknown Pin argument received")
 
 counter = 0
 led_pin = Pin(22, Pin.OUT)
 
-def blink_led():
+def handle_blink_led():
   global counter
   counter += 1
   led_pin.value(counter % 2)
@@ -80,7 +81,9 @@ def blink_led():
 def initialise():
   for pin_number in pin_numbers:
     pin = Pin(pin_number, Pin.IN, Pin.PULL_UP)
-    pin.irq(lambda p:handle_pin(p), trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING)
+    pin.irq(lambda p:handle_pin_change(p),
+              trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING)
     pins.append(pin)
 
-  event.add_event_handler(blink_led, 500)
+  event.add_event_handler(handle_pins_active, 50)  # 20 Hz
+  event.add_event_handler(handle_blink_led, 500)
--- configuration/main.py
+++ configuration/main.py
@@ -1,8 +1,9 @@
 # configuration/main.py: version: 2018-04-30 00:00
 
 settings = {
-  "application":     "aiko/demonstration",  # Application to run
-# "lolibot":         "lolibot",
+  "application":     "aiko/demonstration",      # Application to run
+# "application":     "applications/joysticks",
+# "application":     "lolibot",
 
   "gc_enabled":       False,  # Display Garbage Collector statistics
   "logger_enabled":   False,  # Display everyone's log output
