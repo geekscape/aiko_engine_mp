90602668eb7d6ca4da56b43a6b99c399cbe05fd5
Andy Gelme <geekscape@gmail.com>
2020-12-11 17:38:24 +1100

    Add "wait_gpio_pin_value()" and "wait_touch_pin_pressed()" to "lib/aiko/test.py".

--- lib/aiko/test.py
+++ lib/aiko/test.py
@@ -4,21 +4,24 @@
 # ~~~~~
 # import aiko.test as test
 # test.echo("(pass test.echo)")
-# test.set_gpio_pin_list([19, 22])   # Set list of GPIO pin numbers
-# test.get_gpio_pin_list()           # Get list of GPIO pin numbers
-# test.set_gpio_pins_mode(0)         # GPIO mode: Output
-# test.set_gpio_pins_mode(1)         # GPIO mode: Input pull-down
-# test.set_gpio_pins_mode(2)         # GPIO mode: Input pull-up
-# test.gpio_pins                     # Check GPIO pins have been initialised
-# test.set_gpio_pins_value(0)        # Set all GPIO pins low
-# test.set_gpio_pins_value(1)        # Set all GPIO pins high
-# test.set_gpio_pin_value(index, 0)  # Set single GPIO pin[index] low
-# test.set_gpio_pin_value(index, 1)  # Set single GPIO pin[index] high
-# test.get_gpio_pins_value()         # Check all GPIO pin input values
+# test.set_gpio_pin_list([19, 22])    # Set list of GPIO pin numbers
+# test.get_gpio_pin_list()            # Get list of GPIO pin numbers
+# test.set_gpio_pins_mode(0)          # GPIO mode: Output
+# test.set_gpio_pins_mode(1)          # GPIO mode: Input pull-down
+# test.set_gpio_pins_mode(2)          # GPIO mode: Input pull-up
+# test.gpio_pins                      # Check GPIO pins have been initialised
+# test.set_gpio_pins_value(0)         # Set all GPIO pins low
+# test.set_gpio_pins_value(1)         # Set all GPIO pins high
+# test.set_gpio_pin_value(index, 0)   # Set single GPIO pin[index] low
+# test.set_gpio_pin_value(index, 1)   # Set single GPIO pin[index] high
+# test.get_gpio_pins_value()          # Check all GPIO pin input values
+# test.wait_gpio_pin_value(index, 0)  # Wait for GPIO pin value to be low
+# test.wait_gpio_pin_value(index, 1)  # Wait for GPIO pin value to be high
 #
-# test.set_touch_pin_list([12, 14])  # Set list of touch pin numbers
-# test.get_touch_pin_list()          # Get list of touch pin numbers
-# test.get_touch_pins_value()        # Check all touch pin input values
+# test.set_touch_pin_list([12, 14])   # Set list of touch pin numbers
+# test.get_touch_pin_list()           # Get list of touch pin numbers
+# test.get_touch_pins_value()         # Check all touch pin input values
+# test.wait_touch_pin_pressed(index)  # Wait for touch pin to be pressed
 #
 # To Do
 # ~~~~~
@@ -26,6 +29,7 @@
 #   as a callable function that starts a background thread
 
 import machine
+from time import sleep_ms
 
 import aiko.common as common
 
@@ -98,7 +102,7 @@ def set_touch_pin_list(_touch_pin_list):
   touch_pin_list = _touch_pin_list
   print("(pass set_touch_pin_list)")
 
-def set_gpio_pins_mode(mode):
+def set_gpio_pins_mode(mode, silent=False):
   global gpio_pin_list, gpio_pins
   gpio_pins = []
 
@@ -116,7 +120,8 @@ def set_gpio_pins_mode(mode):
         return
     gpio_pins.append(gpio_pin)
 
-  print("(pass set_gpio_pins_mode)")
+  if not silent:
+    print("(pass set_gpio_pins_mode)")
 
 def set_gpio_pin_value(index, value):
   gpio_pins[index].value(value)
@@ -126,5 +131,22 @@ def set_gpio_pins_value(value):
   for gpio_pin in gpio_pins:
     gpio_pin.value(value)
   print("(pass set_gpio_pins_value)")
+
+def wait_gpio_pin_value(index, value):
+  mode = 2  # IN, PULL_UP
+  if value:
+    mode = 1  # IN, PULL_DOWN
+  set_gpio_pins_mode(mode, silent=True)
+  gpio_pin = gpio_pins[index]
+  while gpio_pin.value() != value:
+    sleep_ms(50)
+  print("(pass wait_gpio_pin_value)")
+
+def wait_touch_pin_pressed(index):
+  touch_pin_number = touch_pin_list[index]
+  touch_pin = machine.TouchPad(machine.Pin(touch_pin_number))
+  while touch_pin.read() > 200:
+    sleep_ms(50)
+  print("(pass wait_touch_pin_pressed)")
 
 print("(pass aiko.test)")
