9e3e1d1ae858bd17fc9d8389deb56d4ea3992dc0
Andy Gelme <geekscape@gmail.com>
2020-12-13 17:44:26 +1100

    Upgrade process: Firmware installation of downloaded files.

--- lib/aiko/upgrade.py
+++ lib/aiko/upgrade.py
@@ -12,6 +12,7 @@
 # - Replace "upgrade_handler()" with proper Aiko Engine button handler
 
 import gc
+import os
 from threading import Thread
 
 import aiko.common as common
@@ -43,13 +44,16 @@ def upgrade_thread():
   try:
     common.log("Firmware upgrade start")
     gc.collect()
-    _, _, host, manifest_pathname = manifest_url.split("/", 3)
 
-#   shutil.path_remove(upgrade_directory)
+    manifest_pathname = "manifest"
+    shutil.path_remove(manifest_pathname)
+# TODO: Remove all "*_new" and "*_old"
 
     aiko.web_client.http_get_file(manifest_url, manifest_pathname)
-#   Verify "manifest_pathname" actual file size versus "manifest_size"
+# TODO: Verify "manifest_pathname" actual file size versus "manifest_size"
+# TODO: Verify "manifest_pathname" checksum
 
+    top_level_files = []
     url_prefix = manifest_url.rpartition("/")[0]
     with open(manifest_pathname, "r") as manifest_file:
       file_index = 0
@@ -58,25 +62,40 @@ def upgrade_thread():
         file_checksum, file_size, filepath = line.split()
         url_suffix = filepath.partition("/")[-1]
         file_url = "/".join([url_prefix, url_suffix])
-        pathname = "/".join([upgrade_directory, url_suffix])
+
+        pathname = url_suffix.partition("/")
+        if not pathname[0] in top_level_files:
+          top_level_files.append(pathname[0])
+        pathname = "".join([pathname[0] + "_new"] + list(pathname[1:]))
+
         print(file_url + " --> " + pathname)
-        common.log("Firmware upgrade %d of %d" % (file_index, file_count))
-
+        common.log("Firmware get ... %d of %d" % (file_index, file_count))
         aiko.web_client.http_get_file(file_url, pathname)
-#       Verify actual file size versus size stated in the "manifest"
+# TODO: Verify actual file size versus size stated in the "manifest"
+# TODO: Verify actual file checksum
 
-#   Move old directories and main.py
-#   Move new directories and main.py
-#   shutil.path_remove(upgrade_directory)
+    shutil.path_remove(manifest_pathname)
+    shutil.file_copy("configuration/net.py",  "configuration_new/net.py")
+    shutil.file_copy("configuration/keys.py", "configuration_new/keys.py")
 
-    version = None
-    common.log("Firmware upgrade success")
+    common.log("Firmware install")
+    for file in top_level_files:
+      try:
+        print("Rename %s to %s" % (file + "_new", file))
+        shutil.path_remove(file)
+        os.rename(file + "_new", file)
+      except OSError:
+        print("OSError")
+
+    common.log("Firmware upgrade success !")
+    common.log("Please reboot :)")
   except Exception as exception:
-    common.log("Firmware upgrade failed")
+    common.log("Firmware upgrade failed :(")
     import sys
     sys.print_exception(exception)
   finally:
     in_progress = False
+    version = None
 
 def on_upgrade_message(topic, payload_in):
   global file_count, manifest_checksum, manifest_size, manifest_url, version
