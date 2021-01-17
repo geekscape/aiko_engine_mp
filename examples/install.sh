7333a6bb9e32fa219550a7cfe623ead82f1e4095
Marc MERLIN <marc_soft@merlins.org>
2021-01-16 16:34:04 -0800

    Better version that knows which files didn't get pushed.

--- examples/install.sh
+++ examples/install.sh
@@ -8,9 +8,18 @@ do
 exec print("$i")
 put $i examples/$i
 EOF
-	touch ."$i".pushed
     fi
 done
 
 # FIXME for your architecture
-test -e  .install.mpf && mpfshell ttyUSB0 -s .install.mpf
+test -e  .install.mpf && mpfshell ttyUSB0 -s .install.mpf 2>&1 | tee .install.mpf.output
+grep -q "Not connected to device" .install.mpf.output && exit
+
+# only update the touch markers if the push was successful
+for i in *.py *.pbm
+do
+    if [ "$i" -nt ."$i".pushed ]; then
+	echo "Pushed $i, updating last push time"
+	touch ."$i".pushed
+    fi
+done
