58c898be47ec9df4cf64699812ceb7b23a6948d7
Marc MERLIN <marc_soft@merlins.org>
2021-01-17 17:26:42 -0800

    Warn if destination directory is missing + compile code before pushing
    also give a hint on how to check free space.

--- examples/install.sh
+++ examples/install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+DEST=examples/
+
 rm -f .install.mpf
+
 for i in *.py *.pbm
 do
     if [ "$i" -nt ."$i".pushed ]; then
+	if [[ $i =~ .py$ ]]; then 
+	    python -m py_compile "$i" || exit
+	fi
 	cat >> .install.mpf << EOF
 exec print("$i")
-put $i examples/$i
+put $i $DEST/$i
 EOF
     fi
 done
@@ -14,6 +20,7 @@ done
 # FIXME for your architecture
 test -e  .install.mpf && mpfshell ttyUSB0 -s .install.mpf 2>&1 | tee .install.mpf.output
 grep -q "Not connected to device" .install.mpf.output && exit
+grep -q "Failed to create file" .install.mpf.output && echo "You will need to manually run md $DEST once" && exit
 
 # only update the touch markers if the push was successful
 for i in *.py *.pbm
@@ -23,3 +30,8 @@ do
 	touch ."$i".pushed
     fi
 done
+
+cat <<EOF
+Type this in repl for space left:
+import os; print(os.statvfs("/")[0]*os.statvfs("/")[3], "bytes free out of",os.statvfs("/")[1]*os.statvfs("/")[2])
+EOF
