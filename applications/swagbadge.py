c67889c5e7f5ed78387402ddc84accdaea0d1599
Andy Gelme <geekscape@gmail.com>
2021-01-21 07:46:27 +1100

    Update "applications/swagbadge.py" to display a simple second timer (since boot).  This is to enable testing of the OLEDProxy and whether Aiko framework special features can play nice and share the OLED screens !

--- applications/swagbadge.py
+++ applications/swagbadge.py
@@ -2,24 +2,39 @@
 #
 # To Do
 # ~~~~~
-# - Implement dual OLED screen support
 # - Implement buttons
 # - Implement touch sliders
 # - Implement Messaging (secure)
 # - Implement SAO handling
 # - Implement Out-Of-The-Box experience
+#   - Put the "images" onto the flash filesystem
+#   - Randomly select 10 images
+#   - Slide them across the OLED screen
+#   - Display Christmas / New Year greeting !
+#   - Repeat :)
 
-import aiko.event as event
-import aiko.oled as oled
+from aiko.common import convert_time
+import aiko.event
+import aiko.oled
 
 titles = ["SwagBadge", "LCA2021"]
 title_index = 0
 
+timer = 0
+
 def swagbadge_handler():
+    global timer
+    hours, minutes, seconds = convert_time(timer)
+    message = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+    aiko.oled.oleds_log(message)
+    timer += 1
+
+def swagbadge_title():
     global title_index
-    oled.set_title(titles[title_index])
-    oled.write_title()
+    aiko.oled.set_title(titles[title_index])
+    aiko.oled.write_title()
     title_index = 1 - title_index
 
 def initialise():
-    event.add_timer_handler(swagbadge_handler, 5000)
+    aiko.event.add_timer_handler(swagbadge_handler, 1000, immediate=True)
+    aiko.event.add_timer_handler(swagbadge_title, 5000)
