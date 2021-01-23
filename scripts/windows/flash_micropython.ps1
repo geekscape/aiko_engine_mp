3b9a117af0e519b92b1776a1b07640b90b5fe612
Alec Clews <alecclews@gmail.com>
2021-01-23 14:18:03 +1100

    Added a Powershell version of flash_micropython

---
+++ scripts/windows/flash_micropython.ps1
@@ -0,0 +1,21 @@
+
+if ([string]::IsNullOrEmpty($env:AMPY_PORT)) {
+    $env:AMPY_PORT = COM9
+}
+
+if ([string]::IsNullOrEmpty($env:BAUDRATE)) {
+    $env:BAUDRATE = 460800
+}
+
+if ([string]::IsNullOrEmpty($env:ESP32_MICROPYTHON)) {
+    $env:ESP32_MICROPYTHON = 'firmware/esp32-idf4-20191220-v1.12.bin'
+}
+
+
+echo "### Erase flash ###"
+esptool.py-script.py --chip esp32 --port $env:AMPY_PORT erase_flash
+
+echo "### Flash microPython ###"
+esptool.py-script.py --chip esp32 --port $env:AMPY_PORT --baud $env:BAUDRATE write_flash -z 0x1000 $env:ESP32_MICROPYTHON
+
+echo "### Complete ###"
