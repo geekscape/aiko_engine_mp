6dfaa53ddc01c73164cc87078e981e9926f6dd4a
Marc MERLIN <marc_soft@merlins.org>
2021-01-24 00:41:48 -0800

    Change neopixel pin to SAO 1, bottom pin.

--- configuration/led.py
+++ configuration/led.py
@@ -3,12 +3,13 @@
 settings = {    # LED panel or string
   "apa106":       False,  # IoTuz and LoliBot use APA106 RGB LEDs
 # "dimension":    (1,),
-  "dimension":    (8,),   # Edge lit acrylic panel
+#  "dimension":    (8,),   # Edge lit acrylic panel
+  "dimension":    (46,),
 # "dimension":    (8, 8),
 # "dimension":    (32, 8),
 # "dimension":    (32, 32),
 # "neopixel_pin":  4,     # TinyPICO
-  "neopixel_pin": 13,     # Usually
+  "neopixel_pin": 19,     # swagbadge SAO 1 bpttom row
 # "neopixel_pin": 15,     # Wemos OLED
 # "neopixel_pin": 23,     # IoTuz
   "zigzag":       False   # For 2D panels
