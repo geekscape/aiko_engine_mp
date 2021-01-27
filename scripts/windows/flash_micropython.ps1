4810771886e0257be2787457dfcad439f557b10c
Alec Clews <alecclews@gmail.com>
2021-01-27 11:24:15 +1100

    Initial support for PowerShell

--- scripts/windows/flash_micropython.ps1
+++ scripts/windows/flash_micropython.ps1
@@ -1,21 +1,21 @@
 
 if ([string]::IsNullOrEmpty($env:AMPY_PORT)) {
-    $env:AMPY_PORT = COM9
+    $env:AMPY_PORT = 'COM9'
 }
 
 if ([string]::IsNullOrEmpty($env:BAUDRATE)) {
-    $env:BAUDRATE = 460800
+    $env:BAUDRATE = '460800'
 }
 
 if ([string]::IsNullOrEmpty($env:ESP32_MICROPYTHON)) {
-    $env:ESP32_MICROPYTHON = 'firmware/esp32-idf4-20191220-v1.12.bin'
+    $env:ESP32_MICROPYTHON = './esp32-idf4-20191220-v1.12.bin'
 }
 
 
 echo "### Erase flash ###"
-esptool.py-script.py --chip esp32 --port $env:AMPY_PORT erase_flash
+py $env:VIRTUAL_ENV\Scripts\esptool.py-script.py --chip esp32 --port $env:AMPY_PORT erase_flash
 
 echo "### Flash microPython ###"
-esptool.py-script.py --chip esp32 --port $env:AMPY_PORT --baud $env:BAUDRATE write_flash -z 0x1000 $env:ESP32_MICROPYTHON
+py $env:VIRTUAL_ENV\Scripts\esptool.py-script.py --chip esp32 --port $env:AMPY_PORT --baud $env:BAUDRATE write_flash -z 0x1000 $env:ESP32_MICROPYTHON
 
 echo "### Complete ###"
